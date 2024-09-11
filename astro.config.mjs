e60d06bca0ab62e5c0f89f62035bcb3c62bfbbe6
Marcus Felix <marcus.felix@gmail.com>
2024-09-11 20:36:44 +0200

    add missing cloudflare secrets

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,8 +15,12 @@ export default defineConfig({
     define: {
       'process.env.STRIPE_SECRET_KEY': JSON.stringify(process.env.STRIPE_SECRET_KEY),
       'process.env.STRIPE_WEBHOOK_SECRET': JSON.stringify(process.env.STRIPE_WEBHOOK_SECRET),
+      'process.env.CUSTOMER_PORTAL_URL': JSON.stringify(process.env.CUSTOMER_PORTAL_URL),
       'process.env.SLACK_TOKEN': JSON.stringify(process.env.SLACK_TOKEN),
-      'process.env.SENDGRID_API_KEY': JSON.stringify(process.env.SENDGRID_API_KEY)
+      'process.env.SENDGRID_API_KEY': JSON.stringify(process.env.SENDGRID_API_KEY),
+      'process.env.SLACK_INVITE_LINK': JSON.stringify(process.env.SLACK_INVITE_LINK),
+      'process.env.DESIGN_PRODUCT_URL': JSON.stringify(process.env.DESIGN_PRODUCT_URL),
+      'process.env.CODE_PRODUCT_URL': JSON.stringify(process.env.CODE_PRODUCT_URL),
     }
   }
 });
